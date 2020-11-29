1d08bd72dee19c44bc206b7a4f4ac47f91f031ed
KEG <eunggyo@KEG.SYS>
2020-11-29 14:05:15 +0900

    프로젝트 업로드

---
+++ app/src/main/java/com/aiden/youtubewebview/MainActivity.java
@@ -0,0 +1,20 @@
+package com.aiden.youtubewebview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+/*
+    Youtube API를 이용한 심플앱
+
+
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
